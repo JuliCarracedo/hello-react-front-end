b5a2b46bc28f6dfdcf852180783a7accf443adb6
JuliCarracedo <julicarra@hotmail.com>
2022-01-26 19:18:03 -0300

    fix fetch request

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Greeting = () => {
     const greet = 'Hello!';
+    const {greetings} = useSelector(store=>store.greetings)
+    console.log(greetings);
+    
     return (<div> 
         <h1>Greetings from all over the world</h1>
         <p> {greet}</p>
--- src/redux/greetings/api.js
+++ src/redux/greetings/api.js
@@ -1,13 +1,18 @@
 import { getGreetingsRequest, getGreetingsSuccess } from "./greetingsReducer"
 
-const URL = 'https://evening-beyond-80457.herokuapp.com/greetings.json'
+const URL = 'https://evening-beyond-80457.herokuapp.com/v1/greetings.json'
 
-const requestFromApi = () => (dispatch) => {
+const requestFromApi = () => async(dispatch) => {
     dispatch(getGreetingsRequest())
-    fetch(URL)
-    .then(response => response.json())
-    .then(json => dispatch(getGreetingsSuccess(json)))
-    .catch(error => console.log(error));
+    const response = await fetch(URL, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+    const obj = await response.json()
+    dispatch(getGreetingsSuccess(obj))
 }
 
 export default requestFromApi;
